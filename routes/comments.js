0df662767f83c6457aca6c0a783bd6a463a02c20
Rose Beazzy <rose.beatty@gmail.com>
2019-12-13 16:58:12 +0100

    adding comments working

--- routes/comments.js
+++ routes/comments.js
@@ -5,17 +5,35 @@ const User = require('../models/User');
 const Comment = require('../models/Comment');
 
 
+router.get('/:userId/:commentId', (req, res) => {
+    const { commentId } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+      }
+
+      Comment.findById(commentId)
+        .then((foundUser) => {
+            console.log(foundUser)
+          res.status(200).json(foundUser)
+        })
+        .catch((err) => res.status(400).json(err))
+})
+
+
 router.post('/create/:id', (req, res) => {
     const { comment, userId } = req.body
 
     Comment.create({comment: comment, userId: userId})
         .then( (newComment) => {
-            console.log(comment);
+            console.log(newComment);
             
             return User.findByIdAndUpdate(userId, { $push: {comments: newComment._id} }, {new: true}).populate('comments')   
         })
         .then((updatedUser) => {
             res.status(201).json(updatedUser)
+            console.log(updatedUser)
         })
         .catch((err) => {
             res.status(400).json(err)
@@ -23,14 +41,13 @@ router.post('/create/:id', (req, res) => {
   })
 
 
+router.put('/update/:id', (req, res) => {
 
-// router.put('/update/:id', (req, res) => {
-
-//     if ( !mongoose.Types.ObjectId.isValid(id)) {
-//         res.status(400).json({ message: 'Specified id is not valid' });
-//         return;
-//       }
-// })
+    if ( !mongoose.Types.ObjectId.isValid(id)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+      }
+})
 
 // router.delete('/delete/:id', (req, res) => {
 //     const { id } = req.params;
