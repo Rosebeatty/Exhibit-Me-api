4b7508321747d82cf93172639b8e7ddda4686dac
Rose Beazzy <rose.beatty@gmail.com>
2019-12-16 16:46:17 +0100

    test15

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(
 // SESSION MIDDLEWARE
 app.use(
   session({
-    secret: 'ironhack',
+    secret: process.env.SESSION_SECRET,
     store: new MongoStore({
       mongooseConnection: mongoose.connection,
       ttl: 24 * 60 * 60, // 1 day
