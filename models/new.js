2f0fe38bbbe91c61dd77c17eb88165e6745c75d7
Rose Beazzy <rose.beatty@gmail.com>
2019-12-16 16:09:29 +0100

    test8

--- models/new.js
+++ models/new.js
@@ -1,1 +1,21 @@
-j
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  email: String,
+  password: String,
+  space_name: String,
+  theme: String,
+  objects: [ {type: Schema.Types.ObjectId, ref:'Model'} ],
+  comments: [{type: Schema.Types.ObjectId, ref:'Comment'}],
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
